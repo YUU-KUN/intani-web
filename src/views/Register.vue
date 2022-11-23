f7853f2bb5484f9286018382e8830c85c0d590bb
Indra <iwahyu@student.telkomuniversity.ac.id>
2022-11-24 00:46:36 +0700

    fixed minor bugs

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,59 +4,74 @@
             <!-- NAME -->
             <div class="text-left mb-5 ">
                 <div class="mb-2 ">
-                    <label for="name">Nama <span class="text-text-red-500 ">*</span></label>
+                    <label for="name">Nama <span class="text-red-500 ">*</span></label>
                 </div>
-                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="text" id="name" placeholder="Ketik nama anda" v-model="name" required/>
+                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="text" id="name"
+                    placeholder="Ketik nama anda" v-model="name" required />
             </div>
-    
+
             <!-- EMAIL -->
             <div class="text-left mb-5 ">
                 <div class="mb-2 ">
-                    <label for="email">Email <span class="text-text-red-500 ">*</span></label>
+                    <label for="email">Email <span class="text-red-500 ">*</span></label>
                 </div>
-                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="email" id="email" placeholder="example@example.com" v-model="email" required/>
+                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="email" id="email"
+                    placeholder="example@example.com" v-model="email" required />
             </div>
-    
+
             <!-- PHONE -->
             <div class="text-left mb-5 ">
                 <div class="mb-2 ">
                     <label for="phone">Nomor HP <span class="text-red-500 ">*</span></label>
                 </div>
-                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="phone" id="phone" placeholder="08xxxxxxxxxxxxx" v-model="phone" required/>
+                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="phone" id="phone"
+                    placeholder="08xxxxxxxxxxxxx" v-model="phone" required />
             </div>
-    
+
             <!-- PASSWORD -->
-            <div class="text-left mb-5 ">
+            <div class="text-left mb-5 password-container">
                 <div class="mb-2 ">
                     <label for="password">Kata Sandi <span class="text-red-500 ">*</span></label>
                 </div>
-                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="password" id="password" placeholder="Pastikan password anda aman ya~" v-model="password" required/>
+                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" :type="password_type" id="password"
+                    placeholder="Pastikan password anda aman" v-model="password" required />
+                <i class="fa-solid" :class="eye_class" id="eye" @click="toggleShowPassword"></i>
+                <!-- <i class="fa-solid fa-eye-slash"></i> -->
             </div>
-    
+
+            <!-- <div class="password-container">
+                <input type="password" placeholder="Password..." id="password">
+                <i class="fa-solid fa-eye" id="eye"></i>
+            </div> -->
+
             <!-- NIK -->
             <div class="text-left mb-5 ">
                 <div class="mb-2 ">
                     <label for="nik">NIK <span class="text-red-500 ">*</span></label>
                 </div>
-                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="password" id="nik" placeholder="Ketik NIK anda" v-model="nik" required/>
+                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="number" id="nik"
+                    placeholder="Ketik NIK anda" v-model="nik" required />
             </div>
-    
+
             <!-- KTP -->
             <div class="text-left mb-5 ">
                 <div class="mb-2 ">
                     <label for="ktp">Scan KTP <span class="text-red-500 ">*</span></label>
                 </div>
-                <input ref="file" class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="file" id="ktp" placeholder="Ketik ktp anda" required @change="selectImage()"/>
+                <input ref="file" class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="file" id="ktp"
+                    placeholder="Ketik ktp anda" required @change="selectImage()" />
             </div>
-    
+
             <!-- FARM GROUP -->
             <div v-if="role == 'farmer'" class="text-left mb-5 ">
                 <div class="mb-2 ">
-                    <label for="farm-group">Kelopmpok Tani</label>
+                    <label for="farm-group">Kelompok Tani</label>
                 </div>
-                <select id="farm-group" class="w-full border-2 border-strong-gray rounded-md py-2 px-3" v-model="farm_group_id">
-                    <option value="" selected disabled>Pilih Kelopmpok Tani</option>
-                    <option v-for="farm_group in farm_groups" :value="farm_group.id" :key="farm_group.id">{{farm_group.name}}</option>
+                <select id="farm-group" class="w-full border-2 border-strong-gray rounded-md py-2 px-3"
+                    v-model="farm_group_id">
+                    <option value="" selected disabled>Pilih Kelompok Tani</option>
+                    <option v-for="farm_group in farm_groups" :value="farm_group.id" :key="farm_group.id">
+                        {{ farm_group.name }}</option>
                 </select>
             </div>
         </div>
@@ -66,9 +81,10 @@
                 <div class="mb-2 ">
                     <label for="name">Nama Bank <span class="text-text-red-500 ">*</span></label>
                 </div>
-                <select id="farm-group" class="w-full border-2 border-strong-gray rounded-md py-2 px-3" v-model="bank_id">
+                <select id="farm-group" class="w-full border-2 border-strong-gray rounded-md py-2 px-3"
+                    v-model="bank_id">
                     <option value="" selected disabled>Pilih bank anda</option>
-                    <option v-for="bank in banks" :value="bank.id" :key="bank.id">{{bank.bank_name}}</option>
+                    <option v-for="bank in banks" :value="bank.id" :key="bank.id">{{ bank.bank_name }}</option>
                 </select>
             </div>
 
@@ -77,7 +93,8 @@
                 <div class="mb-2 ">
                     <label for="name">Nomor Rekening <span class="text-text-red-500 ">*</span></label>
                 </div>
-                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="number" id="name" placeholder="Pastikan nomor rekening benar ya~" v-model="account_number" required/>
+                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="number" id="name"
+                    placeholder="Pastikan nomor rekening benar ya~" v-model="account_number" required />
             </div>
 
             <!-- BANK USER NAME -->
@@ -85,13 +102,14 @@
                 <div class="mb-2 ">
                     <label for="name">Nama Pemiliki Rekening <span class="text-text-red-500 ">*</span></label>
                 </div>
-                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="text" id="name" placeholder="Ketik nama pemilik rekening" v-model="account_name" required/>
+                <input class="w-full border-2 border-strong-gray rounded-md py-2 px-3" type="text" id="name"
+                    placeholder="Ketik nama pemilik rekening" v-model="account_name" required />
             </div>
         </div>
         <CTAButton v-if="state == first" @click="state = second; getAllBanks()">Lanjut</CTAButton>
         <CTAButton v-else @click="register">Daftar</CTAButton>
 
-        
+
     </div>
 </template>
 
@@ -119,7 +137,10 @@ export default {
             bank_id: '',
 
             currentImage: '',
-            previewImage: ''
+            previewImage: '',
+
+            password_type: 'password',
+            eye_class: 'fa-eye',
 
         }
     },
@@ -157,7 +178,7 @@ export default {
             if (this.role == 'farmer') {
                 formData.append('farm_group_id', this.farm_group_id)
             }
-            
+
             formData.append('bank_id', this.bank_id)
             formData.append('account_number', this.account_number)
             formData.append('account_name', this.account_name)
@@ -168,13 +189,17 @@ export default {
                     email: this.email,
                     password: this.password
                 }
-                this.$router.push({name: 'Confirm Register', params: {user}})
+                this.$router.push({ name: 'Confirm Register', params: { user } })
                 // this.$store.dispatch('login', user).then(() => {
                 //     this.$router.push({name: 'Confirm Register', params: {user}})
                 // })
             }).catch(error => {
                 console.log(error.response);
             })
+        },
+        toggleShowPassword() {
+            this.eye_class = this.eye_class == 'fa-eye' ? 'fa-eye-slash' : 'fa-eye'
+            this.password_type = this.password_type == 'password' ? 'text' : 'password'
         }
     },
     mounted() {
@@ -183,6 +208,26 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.password-container {
+    /* width: 400px; */
+    position: relative;
+}
 
+.password-container input[type="password"],
+.password-container input[type="text"] {
+    width: 100%;
+    padding: 12px 36px 12px 12px;
+    box-sizing: border-box;
+}
+
+.fa-solid {
+    position: absolute;
+    /* top: 28%; */
+    top: 60%;
+    /* top: 15%; */
+    right: 8%;
+    cursor: pointer;
+    color: lightgray;
+}
 </style>
